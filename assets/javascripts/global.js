63cf057a463827ce6140eb323ddab0543d59ea87
isomerx <dhruvbakshi12343@gmail.com>
2021-09-10 01:59:25 +0530

    minor-major

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,69 +1,70 @@
-const cursor = document.getElementById("circle");
-document.addEventListener("DOMContentLoaded", () => {
-  let mousePosX = 0,
-    mousePosY = 0,
-    mouseCircle = document.getElementById("circle");
-
-  document.onmousemove = (e) => {
-    mousePosX = e.pageX - cursor.offsetHeight / 2;
-    mousePosY = e.pageY - cursor.offsetHeight / 2;
-  };
-
-  let delay = 6,
-    revisedMousePosX = 0,
-    revisedMousePosY = 0;
-
-  function delayMouseFollow() {
-    requestAnimationFrame(delayMouseFollow);
-
-    revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-    revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
-
-    mouseCircle.style.top = revisedMousePosY + "px";
-    mouseCircle.style.left = revisedMousePosX + "px";
-  }
-  delayMouseFollow();
-});
-
-document.addEventListener("click", () => {
-  cursor.classList.add("expand");
-  setTimeout(() => {
-    cursor.classList.remove("expand");
-  }, 500);
-});
-
 function myFunction(x) {
   x.classList.toggle("change");
   document.getElementById("ext-menu").classList.toggle("show-menu");
 }
 
-$(function () {
-  $("a").hover(
-    function () {
-      $("#circle").animate({ height: "70px", width: "70px" }, 200);
-    },
-    function () {
-      $("#circle").animate({ height: "50px", width: "50px" }, 100);
-    }
-  );
-});
-$(function () {
-  $(".hamburger2").hover(
-    function () {
-      $("#circle").animate({ height: "70px", width: "70px" }, 200);
-    },
-    function () {
-      $("#circle").animate({ height: "50px", width: "50px" }, 100);
-    }
-  );
-});
-$(function () {
-  $(".row").hover(
-    function () {
-      $("#circle").animate({ height: "70px", width: "70px" }, 200);
-    },
-    function () {
-      $("#circle").animate({ height: "50px", width: "50px" }, 100);
-    }
-  );
-});
+// const cursor = document.getElementById("circle");
+// document.addEventListener("DOMContentLoaded", () => {
+//   let mousePosX = 0,
+//     mousePosY = 0,
+//     mouseCircle = document.getElementById("circle");
+
+//   document.onmousemove = (e) => {
+//     mousePosX = e.pageX - cursor.offsetHeight / 2;
+//     mousePosY = e.pageY - cursor.offsetHeight / 2;
+//   };
+
+//   let delay = 6,
+//     revisedMousePosX = 0,
+//     revisedMousePosY = 0;
+
+//   function delayMouseFollow() {
+//     requestAnimationFrame(delayMouseFollow);
+
+//     revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+//     revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+//     mouseCircle.style.top = revisedMousePosY + "px";
+//     mouseCircle.style.left = revisedMousePosX + "px";
+//   }
+//   delayMouseFollow();
+// });
+
+// document.addEventListener("click", () => {
+//   cursor.classList.add("expand");
+//   setTimeout(() => {
+//     cursor.classList.remove("expand");
+//   }, 500);
+// });
+
+
+// $(function () {
+//   $("a").hover(
+//     function () {
+//       $("#circle").animate({ height: "70px", width: "70px" }, 200);
+//     },
+//     function () {
+//       $("#circle").animate({ height: "50px", width: "50px" }, 100);
+//     }
+//   );
+// });
+// $(function () {
+//   $(".hamburger2").hover(
+//     function () {
+//       $("#circle").animate({ height: "70px", width: "70px" }, 200);
+//     },
+//     function () {
+//       $("#circle").animate({ height: "50px", width: "50px" }, 100);
+//     }
+//   );
+// });
+// $(function () {
+//   $(".row").hover(
+//     function () {
+//       $("#circle").animate({ height: "70px", width: "70px" }, 200);
+//     },
+//     function () {
+//       $("#circle").animate({ height: "50px", width: "50px" }, 100);
+//     }
+//   );
+// });
